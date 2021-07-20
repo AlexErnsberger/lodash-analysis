0883e1fff8b8d9c083d1e798441769c11a8fd3c2
禾谦 <q601479@gmail.com>
2021-07-20 17:02:00 +0800

    feat: deburr源码分析

--- deburr.js
+++ deburr.js
@@ -4,10 +4,15 @@ import deburrLetter from './.internal/deburrLetter.js'
 const reLatin = /[\xc0-\xd6\xd8-\xf6\xf8-\xff\u0100-\u017f]/g
 
 /** Used to compose unicode character classes. */
+// https://www.unicode.org/charts/PDF/U0300.pdf
 const rsComboMarksRange = '\\u0300-\\u036f'
+// https://www.unicode.org/charts/PDF/UFE20.pdf
 const reComboHalfMarksRange = '\\ufe20-\\ufe2f'
+// https://www.unicode.org/charts/PDF/U20D0.pdf
 const rsComboSymbolsRange = '\\u20d0-\\u20ff'
+// https://www.unicode.org/charts/PDF/U1AB0.pdf
 const rsComboMarksExtendedRange = '\\u1ab0-\\u1aff'
+// https://www.unicode.org/charts/PDF/U1DC0.pdf
 const rsComboMarksSupplementRange = '\\u1dc0-\\u1dff'
 const rsComboRange = rsComboMarksRange + reComboHalfMarksRange + rsComboSymbolsRange + rsComboMarksExtendedRange + rsComboMarksSupplementRange
 
@@ -26,7 +31,8 @@ const reComboMark = RegExp(rsCombo, 'g')
  * and [Latin Extended-A](https://en.wikipedia.org/wiki/Latin_Extended-A)
  * letters to basic Latin letters and removing
  * [combining diacritical marks](https://en.wikipedia.org/wiki/Combining_Diacritical_Marks).
- *
+ *  以Latin-1 Supplement和Latin Extended-A为标准把字符串转换为基本拉丁字母，并且删除特殊符号，只保留拉丁字母dd
+ * 
  * @since 3.0.0
  * @category String
  * @param {string} [string=''] The string to deburr.
