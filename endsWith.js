e3ae9ebf05aaa5d90397f14fa4fc9f21c49e17fb
禾谦 <q601479@gmail.com>
2021-08-09 15:26:23 +0800

    feat: endwith源码分析

--- endsWith.js
+++ endsWith.js
@@ -22,16 +22,22 @@
  */
 function endsWith(string, target, position) {
   const { length } = string
+  // 是否传position 默认为 +string.length
   position = position === undefined ? length : +position
+  // 负数或NaN
   if (position < 0 || position != position) {
     position = 0
   }
+  // 大于length
   else if (position > length) {
     position = length
   }
+  // 结束位置
   const end = position
+  // 起始位置
   position -= target.length
   return position >= 0 && string.slice(position, end) == target
 }
 
+console.log(endsWith('abc', 'ab', 2))
 export default endsWith
--- keys.js
+++ keys.js
@@ -31,10 +31,71 @@ import isArrayLike from './isArrayLike.js'
 function keys(object) {
   // 类数组对象 不是一个函数 并且 包含一个length属性 最小值为0 最大值为Number.MAX_SAFE_INTEGER
   return isArrayLike(object)
-    // 默认不获取原型链上的属性 
+    // 默认不获取原型链上的属性
     ? arrayLikeKeys(object)
     // 调用Object.keys 返回自身可枚举属性
     : Object.keys(Object(object))
 }
 
-export default keys
+// export default keys
+
+// function useState(initState) {
+//   let _val = initState
+//   const setVal = (newVal) => {
+//     _val = newVal
+//   }
+//   return [_val, setVal]
+// }
+
+
+// const [val, setVal] = useState(1)
+// setVal(2)
+// console.log(val)
+
+
+const React= (function() {
+  let _val;
+  return {
+    useState: function(initValue) {
+      _val = _val || initValue
+      function setVal(newVal) {
+        _val = newVal
+      }
+      return [_val, setVal]
+    }
+  }
+})()
+
+const [val, setVal] = React.useState(1)
+setVal(2)
+console.log(val)
+
+const MyReact = (function() {
+  let _val // hold our state in module scope
+  return {
+    render(Component) {
+      const Comp = Component()
+      Comp.render()
+      return Comp
+    },
+    useState(initialValue) {
+      _val = _val || initialValue // assign anew every run
+      function setState(newVal) {
+        _val = newVal
+      }
+      return [_val, setState]
+    },
+  }
+})()
+
+function Counter() {
+  const [count, setCount] = MyReact.useState(0)
+  return {
+    click: () => setCount(count + 1),
+    render: () => console.log('render:', { count }),
+  }
+}
+let App
+App = MyReact.render(Counter) // render: { count: 0 }
+App.click()
+App = MyReact.render(Counter) // render: { count: 1 }
